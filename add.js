2ad246a0e9885e5a5cc81d162f43b919bbaf6d14
DELL <sulaimabaliqis@gmail.com>
2024-04-18 14:52:03 +0100

    task

--- add.js
+++ add.js
@@ -5,3 +5,11 @@ function evenNumbers(numbers){
     return numbers.filter(number => number % 2 === 0);
 }
 module.exports = {addNumbers, evenNumbers};
+
+let array = [1, 2, 3,4, 5]
+array.forEach((number) => { console.log(number * 2);});
+
+let array1 = [1, 2, 3,4, 5];
+let arr =[];
+array1.forEach((number) => { arr.push(number * 2);});
+console.log(arr);
