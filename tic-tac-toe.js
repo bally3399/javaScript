94864ef84b542aab50298771f1d33becae3b0edd
DELL <sulaimabaliqis@gmail.com>
2024-04-18 20:20:48 +0100

    task

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,14 @@
-function ticTacToe(obj){
-    return obj.map(value => value.value)
+function ticTacToe(obj, length, width){
+    let arr = [];
+    for (let index = 0; index < length; index++) {
+        arr.push([]);
+        for (let idx = 0; idx < width; idx++) {
+            arr[index].push(" ");
+        }
+     }
+    obj.map(num => arr[num.row][num.column]= num.value);
+    return arr;
 }
-
 const length = 3;
 const width = 3;
 const obj = [
@@ -14,13 +21,7 @@ const obj = [
     {row : 2, column : 1, value : "O"},
     {row : 2, column : 2, value : "O"},
 ];
+console.log(ticTacToe(obj, length, width));
+
 module.exports = ticTacToe
-// function printBoard(board) {
-//     for (let index = 0; index < length; index++) {
-//         for (let idx = 0; idx < width; idx++) {
-//             console.log(board[index][idx]);
-//         }
-//         console.log(obj);
-//     }
-// }
-// // printBoard(obj);
+
